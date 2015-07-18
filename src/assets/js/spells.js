6d10810058e963bf4b4ce4cba2b4ec7b146bf8f1
firiz <f.sahafzadeh@gmail.com>
2015-07-19 00:34:00 +0430

    scroll bug fix

--- src/assets/js/spells.js
+++ src/assets/js/spells.js
@@ -10,53 +10,27 @@ $(document).ready(function() {
 	window.sr = new scrollReveal();
 
 	var mapCanvas = document.getElementById('map-canvas');
-  var latLng = new google.maps.LatLng(35.724892, 51.386467);
+	var latLng = new google.maps.LatLng(35.724892, 51.386467);
 	var mapOptions = {
 		center: latLng,
 		zoom: 18,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
-    scrollwheel: false
+		scrollwheel: false
 	};
 	var map = new google.maps.Map(mapCanvas, mapOptions);
-  var marker = new google.maps.Marker({
-      position: latLng,
-      map: map,
-      title: 'دانشکده مهندسی برق و کامپیوتر'
-  });
-
-	/*$(window).scroll(function () {
-	  var currentDiv;
-	  $('section.section').each(function () {
-	    var div = $(this);
-	    if (div.position().top - $(window).scrollTop() <= 0) {
-	      currentDiv = div;
-	    }
-	  });
-	  var activeDivId = $('ul.navigation li.active').attr('id').substring(3);
-	  if (currentDiv && currentDiv.attr('id') !== activeDivId) {
-	    $('ul.navigation li').removeClass('active');
-	    var liQuery = 'ul.navigation li#li-' + currentDiv.attr('id');
-	    var li = $(liQuery);
-	    li.addClass('active');
-	    var id = currentDiv.attr('id');
-	    currentDiv.removeAttr('id');
-	    location.hash = id;
-	    currentDiv.attr('id', id);
-	    ga('send', 'event', 'navigation', id);
-	  }
-
-	  if ($(window).scrollTop() + $(window).height() === $(document).height()) {
-	    $('ul.navigation li').removeClass('active');
-	    var li = $('ul.navigation li#li-contact');
-	    li.addClass('active');
-	    var contact = $('#contact');
-	    contact.removeAttr('id');
-	    location.hash = 'contact';
-	    contact.attr('id', 'contact');
-	    ga('send', 'event', 'navigation', 'contact');
-	  }
+	var marker = new google.maps.Marker({
+		position: latLng,
+		map: map,
+		title: 'دانشکده مهندسی برق و کامپیوتر'
 	});
-  */
+
+	var $body = $(document);
+	$body.bind('scroll', function() {
+		// "Disable" the horizontal scroll.
+		if ($body.scrollLeft() !== 0) {
+			$body.scrollLeft(0);
+		}
+	});
 
 	$('.menu li a').click(function(e) {
 		e.preventDefault();
